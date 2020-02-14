4dc3ec15efce56f60378397f25c4e1f2616945bc
Kevin <6500490+Zeturic@users.noreply.github.com>
2020-02-13 22:24:56 -0500

    gSpeciesNames indirection

--- include/data.h
+++ include/data.h
@@ -1,3 +1,6 @@
 #pragma once
 
-extern const u8 gSpeciesNames[][POKEMON_NAME_LENGTH + 1];
+// extern const u8 gSpeciesNames[][POKEMON_NAME_LENGTH + 1];
+typedef const u8 gSpeciesNamesType[][POKEMON_NAME_LENGTH + 1];
+extern const gSpeciesNamesType * const gSpeciesNamesPtr;
+#define gSpeciesNames (*gSpeciesNamesPtr)
